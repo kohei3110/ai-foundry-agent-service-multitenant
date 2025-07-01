73da8c1349cdbcc11c105a8e59bc16e0b838e2c7
Jason <ngjason@microsoft.com>
2025-07-01 18:06:24 +0800

    added multi tenant AKS example

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt update
+apt install python3 -y
+apt install python3-pip -y
+pip install azure-identity --break-system-packages
+pip install azure-storage-blob --break-system-packages
